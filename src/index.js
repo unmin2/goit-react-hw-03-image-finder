cda64e3ccd412a976b6f042ad9e63e77b6b92241
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-09-01 18:02:39 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App  from './components/App';
-import './GlobalStyle'
+import {GlobalStyle}  from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>
 );
